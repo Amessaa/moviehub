c494d643492dfb182b984e2fb561a0dd1b407c04
Kriti Arora <102133817+gitkriti19@users.noreply.github.com>
2023-06-23 22:39:15 +0530

    script.js

--- script.js
+++ script.js
@@ -33,3 +33,67 @@ ball.addEventListener("click", () => {
   });
   ball.classList.toggle("active");
 });
+
+
+//searching...
+
+const movieSearchBox = document.getElementById('movie-search-box');
+const searchList = document.getElementById('search-list');
+
+// load movies from API
+async function loadMovies(searchTerm){
+    const URL = `https://omdbapi.com/?s=${searchTerm}&page=1&apikey=fc1fef96`;
+    const res = await fetch(`${URL}`);
+    const data = await res.json();
+    //console.log(data.Search);
+     if(data.Response == "True") displayMovieList(data.Search);
+}
+
+function findMovies(){
+    let searchTerm = (movieSearchBox.value).trim();
+    if(searchTerm.length > 0){
+        searchList.classList.remove('hide-search-list');
+        loadMovies(searchTerm);
+    } else {
+        searchList.classList.add('hide-search-list');
+    }
+}
+
+function displayMovieList(movies){
+    searchList.innerHTML="";
+    for(let idx = 0; idx < movies.length; idx++){
+        let movieListItem = document.createElement('div');
+        movieListItem.dataset.id = movies[idx].imdbID; // setting movie id in  data-id
+        movieListItem.classList.add('search-list-item');
+        if(movies[idx].Poster != "N/A")
+            moviePoster = movies[idx].Poster;
+        else 
+            moviePoster = "https://www.vtechkids.com/assets/images/default/image_not_found.png";
+
+        movieListItem.innerHTML = `
+        <div class = "search-item-thumbnail">
+            <img src = "${moviePoster}">
+        </div>
+        <div class = "search-item-info">
+            <h3>${movies[idx].Title}</h3>
+            <p>${movies[idx].Year}</p>
+        </div>
+        `;
+        searchList.appendChild(movieListItem);
+    }
+
+    loadMovieDetails();
+}
+
+function loadMovieDetails(){
+    const searchListMovies = searchList.querySelectorAll('.search-list-item');
+    searchListMovies.forEach(movie => {
+        movie.addEventListener('click', function(){
+            const app = movie;
+            open(`result.html?title=${movie.dataset.id}`);
+        })
+    });
+}
+
+
+
